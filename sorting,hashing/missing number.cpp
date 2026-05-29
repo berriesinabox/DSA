af021ec0ba0562dd90e88267dc80689cce8623b3
Shivangi Shekhar <shivangishekharjha@gmail.com>
2026-05-29 12:39:03 +0530

    Update missing number.cpp

--- sorting,hashing/missing number.cpp
+++ sorting,hashing/missing number.cpp
@@ -1,3 +1,5 @@
+//brute force from my end
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -10,5 +12,20 @@ public:
             return i;
         }
         return -1;
+    }
+};
+
+//optimal solution
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n= nums.size();
+        int sum = n*(n+1)/2;
+
+        for(int num: nums){
+            sum = sum - num;
+        }
+        return sum;
     }
 };
