d0ac3006e2db86d621ff3f1847192077234dd0af
Shivangi Shekhar <shivangishekharjha@gmail.com>
2026-06-01 11:08:19 +0530

    Create Minimum Cost of Buying Candies With Discount.cpp

---
+++ sorting,hashing/Minimum Cost of Buying Candies With Discount.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.rbegin(), cost.rend());
+        int sum =0;
+
+        for(int i =0; i<cost.size(); i++) {
+            if(i %3 !=2) {
+                sum +=cost[i];
+            }
+        }
+        return sum;
+    }
+};
