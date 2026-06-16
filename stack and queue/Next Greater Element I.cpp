3075a1b857bd43da02d85d72c2da7f2098fec525
Shivangi Shekhar <shivangishekharjha@gmail.com>
2026-06-16 08:41:11 +0530

    Update Next Greater Element I.cpp

--- stack and queue/Next Greater Element I.cpp
+++ stack and queue/Next Greater Element I.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> ans;
+        unordered_map<int,int> mpp;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty()  && st.top()<=nums2[i]){
+                st.pop();
+            }
 
+            mpp[nums2[i]]=st.empty() ? -1 : st.top();
+
+            st.push(nums2[i]);
+        }
+
+        for(int i : nums1){
+            if(mpp.find(i) != mpp.end()){
+                ans.push_back(mpp[i]);
+            }
+        }
+        return ans;
+    }
+};
